41db8009836e44cff8085469c621e91314d4662f
yousri-meftah <yousri.meftah1@gmail.com>
2024-02-14 21:55:15 +0100

    navbar and hero section are added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
-
+import styles from "./style"
+import NavBar from "./component/NavBar"
+import Hero from "./component/Hero"
 function App() {
   return (
-    <div>App</div>
+    <div className=" w-full bg-primary ">
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <NavBar />
+      </div>
+    </div>
+
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Hero />
+      </div>
+    </div>
+    </div>
   )
 }
 
